7ac4b78a991fb8023548469c352b039687543b91
Matthew Wei <matthewwei35@gmail.com>
2022-06-09 08:15:58 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Initialize express
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
-// require handlebars
-app.use(express.static('public'));
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
+// require handlebars
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
@@ -73,6 +75,28 @@ app.get('/events/:id', (req, res) => {
     // if they id was for an event not in our db, log an error
     console.log(err.message);
   })
+});
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event: event });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 
 // Choose a port to listen on
